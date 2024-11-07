40225ce4aae0109cc06a82a49272f875d73090f7
Cristi <cristian.dima@uipath.com>
2024-11-07 14:28:50 +0200

    Simplify ISerializable implementation

--- src/UiPath.Workflow.Runtime/ExtensionRequiredException.cs
+++ src/UiPath.Workflow.Runtime/ExtensionRequiredException.cs
@@ -8,35 +8,28 @@ public class ExtensionRequiredException : Exception
 {
     private const string RequiredExtensionTypeName = "requiredExtensionType";
 
-    public string RequiredExtensionTypeFullName { get; }
+    public string RequiredExtensionTypeFullName => Data[RequiredExtensionTypeName] as string;
 
     public ExtensionRequiredException(Type requiredType)
         : base()
     {
-        RequiredExtensionTypeFullName = requiredType.FullName;
+        Data.Add(RequiredExtensionTypeName, requiredType.FullName);
     }
 
     public ExtensionRequiredException(Type requiredType, string message)
         : base(message)
     {
-        RequiredExtensionTypeFullName = requiredType.FullName;
+        Data.Add(RequiredExtensionTypeName, requiredType.FullName);
     }
 
     public ExtensionRequiredException(Type requiredType, string message, Exception innerException)
         : base(message, innerException)
     {
-        RequiredExtensionTypeFullName = requiredType.FullName;
+        Data.Add(RequiredExtensionTypeName, requiredType.FullName);
     }
 
     public ExtensionRequiredException(SerializationInfo info, StreamingContext context)
         : base(info, context)
     {
-        RequiredExtensionTypeFullName = info.GetString(RequiredExtensionTypeName);
-    }
-
-    public override void GetObjectData(SerializationInfo info, StreamingContext context)
-    {
-        base.GetObjectData(info, context);
-        info.AddValue(RequiredExtensionTypeName, RequiredExtensionTypeFullName, typeof(string));
     }
 }
